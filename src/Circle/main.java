6a4bcbf8e7414c533570ff3a8fe55b67b62f41d8
johan1031 <johan.carrby@gmail.com>
2017-11-08 13:59:18 +0100

    Gjorde så att Cirkeln translatas i lämplig riktning när man trycker på w,a,s,d

--- src/Circle/main.java
+++ src/Circle/main.java
@@ -3,6 +3,7 @@ package Circle;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -16,13 +17,34 @@ public class main extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		
-	        Circle circ = new Circle(40);
+		    Circle circ = new Circle(40);
+	        
+	        Group root = new Group(circ);
+	        Scene scene = new Scene(root, WorldWidth, WorldHigth);
 	        
 	        circ.setTranslateX(WorldWidth/2);
 	        circ.setTranslateY(WorldHigth/2);
 	        
-	        Group root = new Group(circ);
-	        Scene scene = new Scene(root, WorldWidth, WorldHigth);
+	        scene.setOnKeyPressed(event->{
+	        	
+	        	if(event.getCode() == KeyCode.W){
+	        		circ.setTranslateY(circ.getTranslateY()-10);
+	        	}
+	        	if(event.getCode() == KeyCode.S){
+	        		circ.setTranslateY(circ.getTranslateY()+10);
+	        	}
+	        	if(event.getCode() == KeyCode.A){
+	        		circ.setTranslateX(circ.getTranslateX()-10);
+	        	}
+	        	if(event.getCode() == KeyCode.D){
+	        		circ.setTranslateX(circ.getTranslateX()+10);
+	        	}
+	        	
+	        	
+	        });
+	        
+	    
+	       
 
 	        stage.setTitle("My JavaFX Application");
 	        stage.setScene(scene);
